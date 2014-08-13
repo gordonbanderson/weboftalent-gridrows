87c3d274dd35359fe7376875b4af8376c0c00917
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-08-13 22:53:05 +0700

    ENHANCEMENT: Allow a grid of the latest items of one particular class type

--- code/GridRowsExtension.php
+++ code/GridRowsExtension.php
@@ -4,17 +4,34 @@ class GridRowsExtension extends DataExtension {
 
   /*
   If you are laying out using some form of grid, e.g. HTML table (ugh) or bootstraps span classes it is useful
-  to have the DataSet split by row.  This is what this method does.
+  to have the DataList split by row.  Here the DataList is generated from a method accessible to the current cotnroller
 
-  To execute this code from a template, do something like the following:
-
-  <% control SplitSetIntoGridRows(Children|4) %>
-  YOur template here
-  <% end_control %>
+  See README.md for a worked example
 
   */
   public function SplitDataListIntoGridRows($itemsInGridMethod,$numberOfCols) {
     $itemsInGrid = $this->owner->$itemsInGridMethod();
+    return $this->createGrid($itemsInGrid,$numberOfCols);
+  }
+
+  /*
+  If you are laying out using some form of grid, e.g. HTML table (ugh) or bootstraps span classes it is useful
+  to have the DataList split by row.  This is what this method does.
+
+  See README.md for a worked example
+
+  */
+  public function SplitClassNameDataListIntoGridRows($className,$numberOfCols,$limit,$sort='LastEdited DESC') {
+    $clazz = Injector::inst()->create($className);
+    $itemsInGrid = $clazz->get()->limit($limit)->sort($sort);
+    return $this->createGrid($itemsInGrid,$numberOfCols);
+  }
+
+
+  /*
+  The actual method that splits the DataList into an ArrayList of rows that contain an ArrayList of Columns
+  */
+  private function createGrid($itemsInGrid,$numberOfCols) {
     $position = 1;
     $columns = new ArrayList();
     $result = new ArrayList();
