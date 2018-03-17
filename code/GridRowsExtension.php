a103fec134c77bfd3b34555c9d9f7d25cb24af22
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-17 23:44:34 +0700

    FIX: PSR2 compliance

--- code/GridRowsExtension.php
+++ code/GridRowsExtension.php
@@ -5,82 +5,85 @@
  */
 class GridRowsExtension extends DataExtension
 {
-	/*
-	If you are laying out using some form of grid, e.g. HTML table (ugh) or
-	bootstraps span classes it is useful to have the DataList split by row.
-	Here the DataList is generated from a method accessible to the current
-	controller
+    /*
+    If you are laying out using some form of grid, e.g. HTML table (ugh) or
+    bootstraps span classes it is useful to have the DataList split by row.
+    Here the DataList is generated from a method accessible to the current
+    controller
 
-	See README.md for a worked example
+    See README.md for a worked example
 
-	*/
-	public function SplitDataListIntoGridRows($itemsInGridMethod, $numberOfCols)
-	{
-		$methodFound = false;
-		$itemsInGrid = null;
+    */
+    public function SplitDataListIntoGridRows($itemsInGridMethod, $numberOfCols)
+    {
+        $methodFound = false;
+        $itemsInGrid = null;
 
-		// Check first the controller and then the model for the method to call
-		if ($this->owner->hasMethod($itemsInGridMethod)) {
-			$itemsInGrid = $this->owner->$itemsInGridMethod();
-			$methodFound = true;
-		}
+        // Check first the controller and then the model for the method to call
+        if ($this->owner->hasMethod($itemsInGridMethod)) {
+            $itemsInGrid = $this->owner->$itemsInGridMethod();
+            $methodFound = true;
+        }
 
-		if (!$methodFound && method_exists($this->owner->model, $itemsInGridMethod)) {
-			$itemsInGrid = $this->owner->model->$itemsInGridMethod();
-			$methodFound = true;
-		}
+        if (!$methodFound && method_exists($this->owner->model, $itemsInGridMethod)) {
+            $itemsInGrid = $this->owner->model->$itemsInGridMethod();
+            $methodFound = true;
+        }
 
-		if ($itemsInGrid == null) {
-			$message = 'Method not found.  A grid cannot be formed from the '
-					 . 'method ' . $itemsInGridMethod;
-			throw new InvalidArgumentException($message);
-		}
+        if ($itemsInGrid == null) {
+            $message = 'Method not found.  A grid cannot be formed from the '
+                     . 'method ' . $itemsInGridMethod;
+            throw new InvalidArgumentException($message);
+        }
 
-		return $this->createGrid($itemsInGrid, $numberOfCols);
-	}
+        return $this->createGrid($itemsInGrid, $numberOfCols);
+    }
 
-	/*
-	If you are laying out using some form of grid, e.g. HTML table (ugh) or
-	bootstraps span classes it is useful to have the DataList split by row.
-	This is what this method does.
+    /*
+    If you are laying out using some form of grid, e.g. HTML table (ugh) or
+    bootstraps span classes it is useful to have the DataList split by row.
+    This is what this method does.
 
-	See USAGE.md for a worked example
+    See USAGE.md for a worked example
 
-	*/
-	public function SplitClassNameDataListIntoGridRows(
-		$className, $numberOfCols, $limit = 10, $sort = 'LastEdited DESC')
-	{
-		$clazz = Injector::inst()->create($className);
-		$itemsInGrid = $clazz->get()->limit($limit)->sort($sort);
-		return $this->createGrid($itemsInGrid, $numberOfCols);
-	}
+    */
+    public function SplitClassNameDataListIntoGridRows(
+        $className,
+        $numberOfCols,
+        $limit = 10,
+        $sort = 'LastEdited DESC'
+    ) {
+        $clazz = Injector::inst()->create($className);
+        $itemsInGrid = $clazz->get()->limit($limit)->sort($sort);
+        return $this->createGrid($itemsInGrid, $numberOfCols);
+    }
 
-	/*
-	The actual method that splits the DataList into an ArrayList of rows that
-	contain an ArrayList of Columns
-	*/
-	private function createGrid($itemsInGrid, $numberOfCols)
-	{
-		$position = 1;
-		$columns = new ArrayList();
-		$result = new ArrayList();
-		foreach ($itemsInGrid as $key => $item) {
-			$columns->push($item);
-			if (($position) >= $numberOfCols) {
-				$position = 1;
-				$row = new ArrayList();
-				$row->Columns = $columns;
-				$result->push($row);
-				$columns = new ArrayList();
-			} else {
-				$position = $position + 1;
-			}
-		}
-		if ($columns->Count() > 0) {
-			$row = new ArrayList();
-			$row->Columns = $columns;
-			$result->push($row);
-		}
-		return $result;
-	}
+    /*
+    The actual method that splits the DataList into an ArrayList of rows that
+    contain an ArrayList of Columns
+    */
+    private function createGrid($itemsInGrid, $numberOfCols)
+    {
+        $position = 1;
+        $columns = new ArrayList();
+        $result = new ArrayList();
+        foreach ($itemsInGrid as $key => $item) {
+            $columns->push($item);
+            if (($position) >= $numberOfCols) {
+                $position = 1;
+                $row = new ArrayList();
+                $row->Columns = $columns;
+                $result->push($row);
+                $columns = new ArrayList();
+            } else {
+                $position = $position + 1;
+            }
+        }
+        if ($columns->Count() > 0) {
+            $row = new ArrayList();
+            $row->Columns = $columns;
+            $result->push($row);
+        }
+        return $result;
+    }
 }
--- tests/GridRowsExtensionTest.php
+++ tests/GridRowsExtensionTest.php
@@ -2,69 +2,70 @@
 
 class GridRowsExtensionTest extends FunctionalTest
 {
-	protected static $fixture_file = 'GridRowsExtensionTest.yml';
+    protected static $fixture_file = 'GridRowsExtensionTest.yml';
 
-	protected $extraDataObjects = array(
+    protected $extraDataObjects = array(
         'GridRowItemTO'
     );
 
-    public function setUp() {
-    	$this->requiredExtensions = array(
-    		'GridRowItemTO' => array('GridRowsExtension')
-    	);
+    public function setUp()
+    {
+        $this->requiredExtensions = array(
+            'GridRowItemTO' => array('GridRowsExtension')
+        );
 
-    	parent::setUp();
+        parent::setUp();
     }
 
 
     public function testSplitDataListMethodDoesNotExist()
     {
-    	$message = 'Method not found.  A grid cannot be formed from the method'
-    			 . ' GridRowItemsTHISMETHODDOESNOTEXIST';
-    	$this->setExpectedException('InvalidArgumentException', $message);
+        $message = 'Method not found.  A grid cannot be formed from the method'
+                 . ' GridRowItemsTHISMETHODDOESNOTEXIST';
+        $this->setExpectedException('InvalidArgumentException', $message);
 
-    	$page = $this->objFromFixture('GridRowItemPageTO', 'page001');
-    	$page->doPublish();
+        $page = $this->objFromFixture('GridRowItemPageTO', 'page001');
+        $page->doPublish();
         $controller = new GridRowItemPageTO_Controller();
         $controller->setDataModel($page);
 
         for ($columns=1; $columns < 15; $columns++) {
-        	$grid = $controller->SplitDataListIntoGridRows(
-        		'GridRowItemsTHISMETHODDOESNOTEXIST', // method from model
-				$columns
-			);
-        	$this->checkGrid($grid, $columns, 10);
+            $grid = $controller->SplitDataListIntoGridRows(
+                'GridRowItemsTHISMETHODDOESNOTEXIST', // method from model
+                $columns
+            );
+            $this->checkGrid($grid, $columns, 10);
         }
     }
 
-	public function testSplitDataListFromModelIntoGridRows()
+    public function testSplitDataListFromModelIntoGridRows()
     {
-    	$page = $this->objFromFixture('GridRowItemPageTO', 'page001');
-    	$page->doPublish();
+        $page = $this->objFromFixture('GridRowItemPageTO', 'page001');
+        $page->doPublish();
         $controller = new GridRowItemPageTO_Controller();
         $controller->setDataModel($page);
 
         for ($columns=1; $columns < 15; $columns++) {
-        	$grid = $controller->SplitDataListIntoGridRows(
-        		'GridRowItems', // method from model
-				$columns
-			);
-        	$this->checkGrid($grid, $columns, 10);
+            $grid = $controller->SplitDataListIntoGridRows(
+                'GridRowItems', // method from model
+                $columns
+            );
+            $this->checkGrid($grid, $columns, 10);
         }
     }
 
     public function testSplitDataListFromControllerIntoGridRows()
     {
-    	$page = $this->objFromFixture('GridRowItemPageTO', 'page001');
-    	$page->doPublish();
+        $page = $this->objFromFixture('GridRowItemPageTO', 'page001');
+        $page->doPublish();
         $controller = new GridRowItemPageTO_Controller();
         $controller->setDataModel($page);
         for ($columns=1; $columns < 15; $columns++) {
-        	$grid = $controller->SplitDataListIntoGridRows(
-        		'GridItems', // method from controller
-				$columns
-			);
-        	$this->checkGrid($grid, $columns, 10);
+            $grid = $controller->SplitDataListIntoGridRows(
+                'GridItems', // method from controller
+                $columns
+            );
+            $this->checkGrid($grid, $columns, 10);
         }
     }
 
@@ -74,67 +75,73 @@ class GridRowsExtensionTest extends FunctionalTest
      */
     public function testSplitClassNameDataListIntoGridRows()
     {
-    	$page = $this->objFromFixture('GridRowItemPageTO', 'page001');
-    	$page->doPublish();
+        $page = $this->objFromFixture('GridRowItemPageTO', 'page001');
+        $page->doPublish();
         $controller = new Page_Controller();
         $controller->setDataModel($page);
         for ($columns=1; $columns < 15; $columns++) {
-        	for ($i=1; $i < 15; $i++) {
-	        	$grid = $controller->SplitClassNameDataListIntoGridRows(
-	        		'GridRowItemTO',
-					$columns,
-					$i,
-					$sort = 'LastEdited DESC');
-	        	$amount = $i > 12 ? 12: $i;
-	        	$this->checkGrid($grid, $columns, $amount);
-	        }
+            for ($i=1; $i < 15; $i++) {
+                $grid = $controller->SplitClassNameDataListIntoGridRows(
+                    'GridRowItemTO',
+                    $columns,
+                    $i,
+                    $sort = 'LastEdited DESC'
+                );
+                $amount = $i > 12 ? 12: $i;
+                $this->checkGrid($grid, $columns, $amount);
+            }
         }
     }
 
 
-    private function checkGrid($grid, $maxWidth, $amount) {
-    	$items = 0;
-    	$rows = 0;
-    	$widths = array();
-    	foreach ($grid->getIterator() as $row) {
-    		$rows++;
-    		$width = 0;
-    		foreach ($row->Columns->getIterator() as $column) {
-    			$items++;
-    			$width++;
-    		}
-    		array_push($widths, $width);
-    	}
+    private function checkGrid($grid, $maxWidth, $amount)
+    {
+        $items = 0;
+        $rows = 0;
+        $widths = array();
+        foreach ($grid->getIterator() as $row) {
+            $rows++;
+            $width = 0;
+            foreach ($row->Columns->getIterator() as $column) {
+                $items++;
+                $width++;
+            }
+            array_push($widths, $width);
+        }
 
-    	// last value will be <= max width
-    	$lastVal = array_pop($widths);
-		$this->assertLessThanOrEqual($maxWidth, $lastVal);
+        // last value will be <= max width
+        $lastVal = array_pop($widths);
+        $this->assertLessThanOrEqual($maxWidth, $lastVal);
 
-    	// All but the last row should equal the expected width, $maxWidth
-    	foreach ($widths as $width) {
-    		$this->assertEquals($maxWidth, $width);
-    	}
-    	$this->assertEquals($amount, $items);
+        // All but the last row should equal the expected width, $maxWidth
+        foreach ($widths as $width) {
+            $this->assertEquals($maxWidth, $width);
+        }
+        $this->assertEquals($amount, $items);
     }
 }
 
 
-class GridRowItemTO extends DataObject implements TestOnly {
-	private static $db = array('Name' => 'Varchar');
+class GridRowItemTO extends DataObject implements TestOnly
+{
+    private static $db = array('Name' => 'Varchar');
 
-	private static $has_one = array('GridRowItemPage' => 'GridRowItemPageTO');
+    private static $has_one = array('GridRowItemPage' => 'GridRowItemPageTO');
 }
 
-class GridRowItemPageTO extends Page implements TestOnly {
-	private static $has_many = array('GridRowItems' => 'GridRowItemTO');
+class GridRowItemPageTO extends Page implements TestOnly
+{
+    private static $has_many = array('GridRowItems' => 'GridRowItemTO');
 }
 
-class GridRowItemPageTO_Controller extends Page_Controller implements TestOnly {
+class GridRowItemPageTO_Controller extends Page_Controller implements TestOnly
+{
 
-	/*
-	This is a test method on the *controller*
-	 */
-	public function GridItems() {
-		return $this->model->GridRowItems();
-	}
+    /*
+    This is a test method on the *controller*
+     */
+    public function GridItems()
+    {
+        return $this->model->GridRowItems();
+    }
 }
